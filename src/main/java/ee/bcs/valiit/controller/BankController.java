c56326c74591903db38b1eacd483f1139a670e7e
Raivo <raivokasepuu@gmail.com>
2020-09-02 13:39:44 +0300

    PostMan_2.09. 13:39

--- src/main/java/ee/bcs/valiit/controller/BankController.java
+++ src/main/java/ee/bcs/valiit/controller/BankController.java
@@ -1,7 +1,10 @@
 package ee.bcs.valiit.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,99 +12,156 @@ import java.util.Map;
 Loo uus controlleri klass BankController
 
 Kasuta staatilist muutujat andmete salvestamiseks
-
 Defineeri rest enpoindid:
-
 createAccount(String accountNr) | loob uue konto etteantud konto
 numbriga
-
 getAccount(String accountNr) | tagasta kui palju raha on vastaval kontol
-
 depositMoney(String accountNr, amount) | kannab loodud kontole raha
 (suurendab kontoga seotud raha muutujat)
-
 withdrawMoney(String accountNr, amount) | võtab kontolt raha (vähendab
 kontol olevat rahasummat)
-
 transferMoney(String account1, String account2, amount) | kanna raha
 esimeselt kontolt teisele kontole
 
 NB:
 @PathVariable  - viide, konto jne
 @RequestBody - data, amount etc, lisame reegline Postmanis json-ina
-@RequestParam - päraast küsimärki url-is
+@RequestParam - pärast küsimärki url-is
  */
 
 @RestController
 public class BankController {
 
-public static final Map<String, Integer> bankAccounts = new HashMap<>();
+    public static final Map<String, Integer> bankAccounts = new HashMap<>();
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     @PostMapping("create/{accountNumber}")
     public void createAccount(@PathVariable String accountNumber) {
-        System.out.println("Account: "+accountNumber+ " created" );
-        bankAccounts.put(accountNumber, 0 );
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account", accountNumber);
+        paramMap.put("balance", 0);
+        int random = (int) ((Math.random() * 500) + 1);
+        paramMap.put("client_id", random);
+        jdbcTemplate.update("INSERT INTO bank_accounts (account_number, balance, client_id) values (:account, :balance, :client_id)",
+                paramMap);
+        System.out.println("Account: " + accountNumber + " created");
+        bankAccounts.put(accountNumber, 0);
         System.out.println("****************************************");
     }
 
     @GetMapping("balance/{accountNumber}")
     public Integer getAccount(@PathVariable String accountNumber) {
-        System.out.println("Account "+ accountNumber+ " has "+ bankAccounts.get(accountNumber)+" dollars");
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account", accountNumber);
+// SQL injection
+// EE123'); DROP TABLE bank_accounts; --,
+// SELECT balance FROM bank_accounts WHERE (account_number = 'EE123');
+        Integer balance = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class);
 
+
+        System.out.println("Account " + accountNumber + " has " + balance + " dollars");
         System.out.println("****************************************");
-        return bankAccounts.get(accountNumber);
+        return balance;
     }
-/*
-    @GetMapping("bank")
-    public String getAllAccount(@PathVariable String accountNumber) {
-        System.out.println("getAccount ");
-        return bankAccounts.toString();
-    }
-    */
-
 
     @PostMapping("deposit/{accountNumber}")
     public void depositMoney(@PathVariable String accountNumber, @RequestBody Integer amount) {
-        System.out.println("depositMoney ");
-        System.out.println("Amount before: " + getAccount(accountNumber));
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account", accountNumber);
+        paramMap.put("balance", amount);
+        Integer before = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class);
+        Integer balance = before + amount;
+        paramMap.put("newbalance", balance);
+        jdbcTemplate.update("UPDATE bank_accounts SET balance = :newbalance WHERE (account_number = :account)", paramMap);
 
-        bankAccounts.put(accountNumber, bankAccounts.get(accountNumber) + amount);
-        System.out.println("Amount after :"+ bankAccounts.get(accountNumber));
+
+        System.out.println("depositMoney ");
+        System.out.println("Amount before: " + before);
+        balance = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class);
+        System.out.println("Amount after :" + balance);
         System.out.println("****************************************");
     }
 
     @PostMapping("withdraw/{accountNumber}")
     public void withdrawMoney(@PathVariable String accountNumber, @RequestBody Integer amount) {
         System.out.println("withdrawMoney");
-        System.out.println("From: " + accountNumber + " before: " + bankAccounts.get(accountNumber));
-        if (bankAccounts.get(accountNumber) > amount) {
-            bankAccounts.put(accountNumber, bankAccounts.get(accountNumber) - amount);
+
+        // Küsime konto seisu:
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account", accountNumber);
+        paramMap.put("amount", amount);
+
+        Integer before = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class);
+        System.out.println("Account: " + accountNumber + " has: " + before);
+        System.out.println("Withdraw request: " + amount);
+
+        if (before > amount) {
+            // teeme withdraw
+            Integer balance = before - amount;
+            paramMap.put("newbalance", balance);
+            jdbcTemplate.update("UPDATE bank_accounts SET balance = :newbalance WHERE (account_number = :account)", paramMap);
+
         } else {
+            //keeldume withdraw-st
             System.out.println("Amount on bankAccount < withdraw request");
         }
-        System.out.println("after: " + bankAccounts.get(accountNumber));
+        Integer after = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class);
+
+        System.out.println("Balance after withdraw: " + after);
         System.out.println("****************************************");
     }
+
+
 
     @PostMapping("transfer/{accountNumber},{toAccountNumber}")
     public void btMoney(@PathVariable String accountNumber, @RequestBody Integer amount, @PathVariable String toAccountNumber) {
         // http://localhost:8080/bankTransfer/1111,100,1112 case sensitive!!!!
-
         System.out.println("transferMoney");
-        System.out.println("From: " + accountNumber + " before: " + bankAccounts.get(accountNumber));
-        System.out.println("To: " + toAccountNumber + " before: " + bankAccounts.get(toAccountNumber));
-        System.out.println("amount: " + amount);
-        //System.out.println("From: " + accountNumber + " before: " + bankAccounts.get(accountNumber));
-        if (bankAccounts.get(accountNumber) > amount) {
-            bankAccounts.put(accountNumber, bankAccounts.get(accountNumber) - amount);
-            bankAccounts.put(toAccountNumber, bankAccounts.get(toAccountNumber) + amount);
+        // Küsime konto seisu:
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account", accountNumber);
+        paramMap.put("amount", amount);
+        paramMap.put("toaccount", toAccountNumber);
+
+        Integer beforeFrom = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class);
+        Integer toAccountBefore = jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :toaccount)",
+                paramMap, Integer.class);
+
+        System.out.println("Account: " + accountNumber + " has: " + beforeFrom);
+        System.out.println("Transfer request amount: " + amount);
+        System.out.println("to account: " + toAccountNumber + "with balance before: " + toAccountBefore);
+        System.out.println("Thinking about it...");
+        System.out.println();
+        if (beforeFrom > amount) {
+            // teeme transferi
+            System.out.println("Pappi jagub!");
+            Integer newBalance = beforeFrom - amount;
+            paramMap.put("newbalance", newBalance);
+            jdbcTemplate.update("UPDATE bank_accounts SET balance = :newbalance WHERE (account_number = :account)", paramMap);
+            Integer new2balance = toAccountBefore + amount;
+            paramMap.put("new2balance", new2balance);
+            jdbcTemplate.update("UPDATE bank_accounts SET balance = :new2balance WHERE (account_number = :toaccount)", paramMap);
+
         } else {
+            //keeldume withdraw-st
             System.out.println("Amount on bankAccount < withdraw request");
         }
-        System.out.println(accountNumber + " after: " + bankAccounts.get(accountNumber));
-        System.out.println(toAccountNumber + "after: " + bankAccounts.get(toAccountNumber));
 
+        System.out.println("From account balance after transfer: " + jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :account)",
+                paramMap, Integer.class));
+        System.out.println("To account balance after transfer: " + jdbcTemplate.queryForObject("SELECT balance FROM bank_accounts WHERE (account_number = :toaccount)",
+                paramMap, Integer.class));
         System.out.println("****************************************");
+
+
     }
 
 
@@ -140,8 +200,6 @@ public static final Map<String, Integer> bankAccounts = new HashMap<>();
     }
 
 */
-
-
 
 
 }
